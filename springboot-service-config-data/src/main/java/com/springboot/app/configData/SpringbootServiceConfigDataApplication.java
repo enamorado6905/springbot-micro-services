1aab1cc33a89882b9cf16f9c864584e74f470a0c
Orlando J Enamorado Rosabal <orlandoenamoradorosabal@gmail.com>
2022-04-22 21:42:14 -0400

    Fix config data

--- springboot-service-config-data/src/main/java/com/springboot/app/configData/SpringbootServiceConfigDataApplication.java
+++ springboot-service-config-data/src/main/java/com/springboot/app/configData/SpringbootServiceConfigDataApplication.java
@@ -2,14 +2,19 @@ package com.springboot.app.configData;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableConfigServer
 @SpringBootApplication
+@EnableConfigServer
+@EnableScheduling
+@EnableAsync
+@EnableCaching
 public class SpringbootServiceConfigDataApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServiceConfigDataApplication.class, args);
 	}
-
 }
